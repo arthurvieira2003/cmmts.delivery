463de4095863d22c3db112c44884dddd99569775
arthurvieira2003 <145725765+arthurvieira2003@users.noreply.github.com>
2024-05-01 20:08:58 -0300

    Rota Login

---
+++ public/js/index.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const data = { username, password };
+
+    fetch("http://merdangrok/Usuarios/Login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Sucesso:", data);
+      })
+      .catch((error) => {
+        console.error("Erro:", error);
+      });
+  });
+});

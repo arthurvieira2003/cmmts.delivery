44fd4ff7aa1fb45c4087a83170bb04c61e35a02f
arthurvieira2003 <arthur.vieira@catolicasc.edu.br>
2024-05-16 14:46:54 -0300

    Testes Mapa

--- public/js/mapa.js
+++ public/js/mapa.js
@@ -1,11 +1,13 @@
 let map;
 let directionsService;
 let directionsRenderer;
+let autocomplete;
+let trafficLayer;
 
 function initMap() {
     // Inicialize o mapa
     map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -26.28468913327435, lng: -48.850313853760944 },
+        center: { lat: -23.55052, lng: -46.633308 },
         zoom: 14
     });
 
@@ -14,15 +16,49 @@ function initMap() {
     directionsRenderer = new google.maps.DirectionsRenderer();
     directionsRenderer.setMap(map);
 
+    trafficLayer = new google.maps.TrafficLayer();
+
+    // Configurar o autocomplete para a barra de pesquisa
+    const input = document.getElementById('pac-input');
+    autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', map);
+
+    // Lidar com a seleção de um lugar da barra de pesquisa
+    autocomplete.addListener('place_changed', onPlaceChanged);
+
     // Adicione um listener para carregar a rota ao clicar no mapa
     map.addListener('click', (event) => {
         const destination = event.latLng;
         calculateAndDisplayRoute(destination);
     });
+
+    document.getElementById('toggle-traffic').addEventListener('click', function () {
+        if (trafficLayer.getMap() === null) {
+            trafficLayer.setMap(map);
+        } else {
+            trafficLayer.setMap(null);
+        }
+    });
+}
+
+function onPlaceChanged() {
+    const place = autocomplete.getPlace();
+
+    if (!place.geometry) {
+        window.alert("Nenhum detalhe disponível para: '" + place.name + "'");
+        return;
+    }
+
+    // Se o lugar tiver geometria, centralize o mapa nele e calcule a rota
+    map.setCenter(place.geometry.location);
+    map.setZoom(17);
+
+    const destination = place.geometry.location;
+    calculateAndDisplayRoute(destination);
 }
 
 function calculateAndDisplayRoute(destination) {
-    const start = { lat: -26.28468913327435, lng: -48.850313853760944 }; // Ponto de partida
+    const start = { lat: -23.55052, lng: -46.633308 }; // Ponto de partida
 
     directionsService.route(
         {

2ae49f75d8b262f10db1087ab330307c273cd13e
RAFAEL RODRIGUES FERREIRA DE ANDRADE <r.andrade@catolicasc.edu.br>
2024-06-12 19:48:42 -0300

    Revert "feat: dashboard alimentado"
    This reverts commit 7e32cedbf9584d4f6d43ea4c3a7223f110872e4b.

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,23 +1,12 @@
-async function BuscarItensDashboard() {
-    try {
-        const response = await fetch('http://localhost:5219/Roteirizador/Dashboard')
-        const data = await response.json();
+async function BuscarItensDashboard(){
+    return fetch('/Dashboard/BuscarItensDashboard')
+    .then(data => {
         return data;
-    } catch {
-        console.error('Fetch error:', error);
-    }
+    })
 }
 
-async function atualizarDashboard() {
-    const dados = await BuscarItensDashboard();
-
-    if (dados) {
-        // maracutaia pra tacar no front
-        document.querySelector('.card:nth-child(1) h2').innerText = dados.entreguesHoje;
-        document.querySelector('.card:nth-child(2) h2').innerText = dados.entregues;
-        document.querySelector('.card:nth-child(3) h2').innerText = dados.naoEntregues;
-        document.querySelector('.card:nth-child(4) h2').innerText = dados.emAndamento;
-    }
-}
-
-window.onload = atualizarDashboard;
+document.addEventListener("DOMContentLoaded", () => {
+    let dadosDashboard = BuscarItensDashboard();
+    console.log(dadosDashboard)
+    document.getElementById('teste').innerText = dadosDashboard.data;
+})

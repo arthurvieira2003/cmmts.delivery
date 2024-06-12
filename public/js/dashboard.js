b0317c663466bf879a41f221e38879536477fa27
arthurvieira2003 <arthur.vieira@catolicasc.edu.br>
2024-06-12 19:57:43 -0300

    Update dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,12 +1,23 @@
-async function BuscarItensDashboard(){
-    return fetch('/Dashboard/BuscarItensDashboard')
-    .then(data => {
+async function BuscarItensDashboard() {
+    try {
+        const response = await fetch('http://localhost:5219/Roteirizador/Dashboard')
+        const data = await response.json();
         return data;
-    })
+    } catch {
+        console.error('Fetch error:', error);
+    }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    let dadosDashboard = BuscarItensDashboard();
-    console.log(dadosDashboard)
-    document.getElementById('teste').innerText = dadosDashboard.data;
-})
+async function atualizarDashboard() {
+    const dados = await BuscarItensDashboard();
+
+    if (dados) {
+        // maracutaia pra tacar no front
+        document.querySelector('.card:nth-child(1) h2').innerText = dados.entreguesHoje;
+        document.querySelector('.card:nth-child(2) h2').innerText = dados.entregues;
+        document.querySelector('.card:nth-child(3) h2').innerText = dados.naoEntregues;
+        document.querySelector('.card:nth-child(4) h2').innerText = dados.emAndamento;
+    }
+}
+
+window.onload = atualizarDashboard;
